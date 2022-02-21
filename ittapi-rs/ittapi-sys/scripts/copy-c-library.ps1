05939c9e78722d22473f08606ca275a45363ddf7
Benjamin Bouvier <public@benj.me>
2022-02-21 11:47:41 +0100

    add an extra level of indirection for the copy-scripts

--- ittapi-rs/ittapi-sys/scripts/copy-c-library.ps1
+++ ittapi-rs/ittapi-sys/scripts/copy-c-library.ps1
@@ -1,7 +1,7 @@
 # Copy the source code of the C library into `ittapi-rs/c-library`. This is a workaround for Windows
 # developers that may not be able to use POSIX symlinks. Also see:
 # https://github.com/git-for-windows/git/wiki/Symbolic-Links.
-$cLibrary = $MyInvocation.MyCommand.Path | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent
+$cLibrary = $MyInvocation.MyCommand.Path | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent
 $rustLibrary = Join-Path -Path $cLibrary -ChildPath "ittapi-rs"
 $linkLocation = Join-Path -Path $rustLibrary -ChildPath "c-library"
 Remove-Item $linkLocation -Force -Recurse
