bcec3e42cdde38641cb68dbd0c6f0e0ec3d8ad82
Andrew Brown <andrew.brown@intel.com>
2024-04-09 02:03:09 -0700

    rust: remove unused code

--- rust/ittapi-sys/src/lib.rs
+++ rust/ittapi-sys/src/lib.rs
@@ -1,4 +1,5 @@
 //! This library contains OS-specific bindings to the C `ittapi` library.
+
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 #![allow(unused)]
 #![deny(clippy::all)]
@@ -32,11 +33,3 @@ include!("windows/jitprofiling_bindings.rs");
 include!("freebsd/jitprofiling_bindings.rs");
 #[cfg(target_os = "openbsd")]
 include!("openbsd/jitprofiling_bindings.rs");
-
-// #[link(name = "ittnotify", kind = "static")]
-// extern "C" {
-//     #[link_name = "__itt_domain_create_init_3_0"]
-//     pub fn __itt_domain_create_init_3_0(name: *const std::os::raw::c_char) -> *mut __itt_domain;
-//     // #[link_name = "__itt_domain_create_init_3_0"]
-//     // pub fn __itt_domain_create(name: *const std::os::raw::c_char) -> *mut __itt_domain;
-// }
