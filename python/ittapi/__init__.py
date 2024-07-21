3bd826c9d2500a3014ea02f66a30d250b364e83b
Srivastava, Manish Kumar <manish.kumar.srivastava@intel.com>
2024-07-21 17:31:22 +0530

    VASP-30439: comments changed

--- python/ittapi/__init__.py
+++ python/ittapi/__init__.py
@@ -14,5 +14,5 @@ from .id import id
 from .string_handle import string_handle
 from .task import NestedTask, OverlappedTask, task, nested_task, overlapped_task
 from .thread_naming import thread_set_name
-# adapter for 
+# adapter for itt-NERSC implementation
 import itt_NERSC
