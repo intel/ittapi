df1ffe6812d1f9e8c455b06d452d2c45544f413d
Srivastava, Manish Kumar <manish.kumar.srivastava@intel.com>
2024-07-27 04:20:56 +0530

    removed redundant functions from module ittapi.compat by existing function of ittapi module

--- python/ittapi/compat/__init__.py
+++ python/ittapi/compat/__init__.py
@@ -2,7 +2,7 @@
 Python binding to NERSC implementation of Intel Instrumentation and Tracing Technology (ITT) API.
 """
 from ittapi.native import pt_region_begin, pt_region_end
-from .collection_control import resume, pause, detach
-from .domain import domain_create
+from ittapi.collection_control import resume, pause, detach
+from ittapi.domain import domain as domain_create
 from .task import task_begin, task_end
 from .pt_region import pt_region_create
